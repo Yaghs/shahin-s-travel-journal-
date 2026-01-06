f8f267aaba36119d94604bd32dd7a7e0beebb57f
Yaghs <syaghinloo@gmail.com>
2026-01-06 16:10:36 -0500

    Parent component App.js calls the child component Entry and passes props by calling the array inside data.js using spread syntax and the JS map method
    data.js has been refactored to pass in props instead of hard coded data
    
    additional css has been added
    
    additional locations added inside data.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Header from "./components/Header.js"
 import Entry from "./components/Entry.js"
+import {data} from "./data.js"
 function App() {
+  const JournalEntry = data.map((entry)=>{
+    return(
+      <Entry 
+        key = {entry.id}
+        {...entry}
+      />
+    )
+  })
   return (
     <>
       <Header/>
-      <Entry />
+      <main className="container">
+        {JournalEntry}
+      </main>
+      
     </>
   );
 }
--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,19 +1,20 @@
 import marker from "./../images/marker.png"
 
 
-export default function Entry(){
+export default function Entry(props){
     return(
-        <article>
-            <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji" />
+        <article className="JournalEntry">
+            <div className="main-image-container">
+               <img className="main-image" src={props.img.src} alt={props.img.alt} /> 
+            </div>
             <div>
                 <img src={marker} alt="Marker"/>
-                <span>JAPAN</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on google maps</a>
+                <span className="country">{props.country}</span>
+                <a href={props.googleMapsLink}>View on google maps</a>
+                <h2 className="entry-title">{props.title}</h2>
+                <p className="trip-dates">{props.dates}</p>
+                <p className="entry-text">{props.text}</p>
             </div>
-            
-
-            <p>12 Jan, 2021 - 24 Jan, 2021</p>
-            <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
         </article>
     )
 
